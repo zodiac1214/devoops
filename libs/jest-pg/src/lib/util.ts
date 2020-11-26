546a0018b8738afafcb8120240a47928126e96dc
Di Zou <dzou@marchnetworks.com>
2020-11-26 11:13:11 -0500

    update jest-pg

--- libs/jest-pg/src/lib/util.ts
+++ libs/jest-pg/src/lib/util.ts
@@ -2,14 +2,16 @@ import { Client } from 'pg';
 import * as dotenv from 'dotenv';
 import * as findFileUp from 'find-file-up';
 
-export const postgresClient: Client = new Client({
-  host: process.env.TYPEORM_HOST,
-  port: process.env.TYPEORM_PORT,
-  database: 'postgres',
-  user: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  ssl: process.env.TYPEORM_SSL === 'true',
-});
+export const postgresClient: Client = () => {
+  return new Client({
+    host: process.env.TYPEORM_HOST,
+    port: process.env.TYPEORM_PORT,
+    database: 'postgres',
+    user: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    ssl: process.env.TYPEORM_SSL === 'true',
+  });
+};
 
 export const loadEnv = () => {
   const envFilePath: string = findFileUp.sync('.env');
